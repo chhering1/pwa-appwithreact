d275542297c8f3c6e4c38d27bc9ea58d666de736
chhering1 <6088255@rts-365.dk>
2021-01-27 14:59:30 +0100

    safari added

--- src/index.js
+++ src/index.js
@@ -24,4 +24,6 @@ serviceWorkerRegistration.register();
 reportWebVitals();
 
 
-OneSignal.initialize('8a753e99-73aa-4f8f-8770-55e767c8e733');
+OneSignal.initialize('8a753e99-73aa-4f8f-8770-55e767c8e733', {
+      safari_web_id: 'web.onesignal.auto.1a1cd43f-104c-4ac2-b069-e07775f1eeb9'
+});

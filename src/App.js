c137734cc39718776bd482347de1670eb272780f
chhering1 <6088255@rts-365.dk>
2021-01-28 08:46:24 +0100

    onesignal updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import OneSignal from 'react-onesignal';
+
+OneSignal.initialize('8a753e99-73aa-4f8f-8770-55e767c8e733', {
+  safari_web_id: 'web.onesignal.auto.1a1cd43f-104c-4ac2-b069-e07775f1eeb9'
+});
 
 function App() {
   return (
--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
-import OneSignal from 'react-onesignal';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -24,6 +23,3 @@ serviceWorkerRegistration.register();
 reportWebVitals();
 
 
-OneSignal.initialize('8a753e99-73aa-4f8f-8770-55e767c8e733', {
-      safari_web_id: 'web.onesignal.auto.1a1cd43f-104c-4ac2-b069-e07775f1eeb9'
-});

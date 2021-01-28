1e9ede24523e97fb696ca8ff572882bb0521db21
chhering1 <6088255@rts-365.dk>
2021-01-28 14:40:23 +0100

    routing test

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 import OneSignal from 'react-onesignal';
+import {Router } from '@reach/router';
+import Home from './components/Home'
+import Page from './components/Page';
 
 OneSignal.initialize('8a753e99-73aa-4f8f-8770-55e767c8e733', {
-  safari_web_id: 'web.onesignal.auto.1a1cd43f-104c-4ac2-b069-e07775f1eeb9'
-});
+    safari_web_id: 'web.onesignal.auto.1a1cd43f-104c-4ac2-b069-e07775f1eeb9'
+  });
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()  {
+    return (
+           <Router>
+              <Home path="/"/>
+              <Page path="/page" />
+           </Router>
+    )
 }
-
 export default App;
---
+++ src/components/Page.js
@@ -0,0 +1,12 @@
+ import image from '../img/image.png';
+
+ const Page = () => {
+     return (  
+         <div>
+               <h2>det er underside</h2>
+               <img src={image} alt=""/>
+         </div>
+     );
+ }
+  
+ export default Page;
